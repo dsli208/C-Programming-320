47975bd89d2e37dbb8d078057a126a84363b913d
David Li <dsli@cs.stonybrook.edu>
2018-05-04 01:33:59 -0400

    Small modifications to correct some mailbox compile errors

--- hw5/src/mailbox.c
+++ hw5/src/mailbox.c
@@ -22,21 +22,54 @@ typedef struct mailbox_entry_block {
 typedef struct mailbox {
     // Reference count
     int reference_count;
+    // Handle
+    char *handle;
     // Entry queue
     ENTRY_BLOCK *head;
     ENTRY_BLOCK *tail;
 } MAILBOX;
 
+// Mailbox helper functions
+void add_mb_entry(MAILBOX *mb, MAILBOX_ENTRY *new_mailbox_entry) { // CHECK
+    if (mb -> head == NULL && mb -> tail == NULL) { // Empty list, consider changing && to ||?
+        // Construct first node
+        mb -> head = malloc(sizeof(ENTRY_BLOCK));
+        mb -> head -> entry = new_mailbox_entry;
+        mb -> head -> next = NULL;
+        mb -> head -> prev = NULL;
+        mb -> tail = mb -> head;
+    }
+    else {
+        ENTRY_BLOCK *new_block = malloc(sizeof(ENTRY_BLOCK));
+        new_block -> entry = new_mailbox_entry;
+        new_block -> next = NULL;
+        new_block -> prev = mb -> tail;
+
+        mb -> tail -> next = new_block;
+        mb -> tail = new_block;
+    }
+}
+
 /*
  * Create a new mailbox for a given handle.
  * The mailbox is returned with a reference count of 1.
  */
-MAILBOX *mb_init(char *handle);
+MAILBOX *mb_init(char *handle) {
+    MAILBOX *new_mailbox = malloc(sizeof(MAILBOX));
+    new_mailbox -> reference_count = 1;
+    new_mailbox -> handle = handle;
+    new_mailbox -> head = NULL;
+    new_mailbox -> tail = NULL;
+
+    return new_mailbox;
+}
 
 /*
  * Set the discard hook for a mailbox.
  */
-void mb_set_discard_hook(MAILBOX *mb, MAILBOX_DISCARD_HOOK *);
+void mb_set_discard_hook(MAILBOX *mb, MAILBOX_DISCARD_HOOK *) {
+
+}
 
 /*
  * Increase the reference count on a mailbox.
@@ -44,7 +77,9 @@ void mb_set_discard_hook(MAILBOX *mb, MAILBOX_DISCARD_HOOK *);
  * so that the reference count always matches the number of pointers
  * that exist to the mailbox.
  */
-void mb_ref(MAILBOX *mb);
+void mb_ref(MAILBOX *mb) {
+    mb -> reference_count += 1;
+}
 
 /*
  * Decrease the reference count on a mailbox.
@@ -53,7 +88,13 @@ void mb_ref(MAILBOX *mb);
  * that exist to the mailbox.  When the reference count reaches zero,
  * the mailbox will be finalized.
  */
-void mb_unref(MAILBOX *mb);
+void mb_unref(MAILBOX *mb) {
+    mb -> reference_count -= 1;
+
+    if (mb -> reference_count <= 0) {
+        // mailbox finalized
+    }
+}
 
 /*
  * Shut down this mailbox.
@@ -63,12 +104,16 @@ void mb_unref(MAILBOX *mb);
  * discarded.  At that point, the mailbox will be finalized, and any
  * entries that remain in it will be discarded.
  */
-void mb_shutdown(MAILBOX *mb);
+void mb_shutdown(MAILBOX *mb) {
+
+}
 
 /*
  * Get the handle associated with a mailbox.
  */
-char *mb_get_handle(MAILBOX *mb);
+char *mb_get_handle(MAILBOX *mb) {
+    return mb -> handle;
+}
 
 /*
  * Add a message to the end of the mailbox queue.
@@ -87,7 +132,25 @@ char *mb_get_handle(MAILBOX *mb);
  * the reference count is performed.  However, after the call the
  * caller must discard this pointer which it no longer "owns".
  */
-void mb_add_message(MAILBOX *mb, int msgid, MAILBOX *from, void *body, int length);
+void mb_add_message(MAILBOX *mb, int msgid, MAILBOX *from, void *body, int length) {
+    MAILBOX_ENTRY *new_entry = malloc(sizeof(MAILBOX_ENTRY));
+    new_entry -> type = MESSAGE_ENTRY_TYPE;
+
+    // Allocate the message
+    MESSAGE *message = malloc(sizeof(MESSAGE));
+
+    // Set message values
+    message -> msgid = msgid;
+    message -> from = from;
+
+    // Set the remaining entry values
+    new_entry -> content.message = *message;
+    new_entry -> body = body;
+    new_entry -> length = length;
+
+    // Add it to the entries queue
+    add_mb_entry(mb, new_entry);
+}
 
 /*
  * Add a notice to the end of the mailbox queue.
@@ -101,7 +164,25 @@ void mb_add_message(MAILBOX *mb, int msgid, MAILBOX *from, void *body, int lengt
  * storage, as it will become the responsibility of whomever removes
  * this notice from the mailbox.
  */
-void mb_add_notice(MAILBOX *mb, NOTICE_TYPE ntype, int msgid, void *body, int length);
+void mb_add_notice(MAILBOX *mb, NOTICE_TYPE ntype, int msgid, void *body, int length) {
+    MAILBOX_ENTRY *new_entry = malloc(sizeof(MAILBOX_ENTRY));
+    new_entry -> type = NOTICE_ENTRY_TYPE;
+
+    // Allocate the notice
+    NOTICE *notice = malloc(sizeof(NOTICE));
+
+    // Set the notice values
+    notice -> type = ntype;
+    notice -> msgid = msgid;
+
+    // Set the remaining entry values
+    new_entry -> content.notice = *notice;
+    new_entry -> body = body;
+    new_entry -> length = length;
+
+    // Add it to the entries queue
+    add_mb_entry(mb, new_entry);
+}
 
 /*
  * Remove the first entry from the mailbox, blocking until there is
@@ -114,4 +195,18 @@ void mb_add_notice(MAILBOX *mb, NOTICE_TYPE ntype, int msgid, void *body, int le
  * The thread servicing the mailbox should use this as an indication
  * that service should be terminated.
  */
-MAILBOX_ENTRY *mb_next_entry(MAILBOX *mb);
+MAILBOX_ENTRY *mb_next_entry(MAILBOX *mb) {
+    if (mb -> head == NULL) {
+        return NULL;
+    }
+    else if (mb -> head == mb -> tail) {
+        ENTRY_BLOCK *mb_entry_block = mb -> head;
+        mb -> head = mb -> tail = NULL;
+        return mb_entry_block -> entry;
+    }
+    else {
+        ENTRY_BLOCK *mb_entry_block = mb -> head;
+        mb -> head = mb -> head -> next;
+        return mb_entry_block -> entry;
+    }
+}
